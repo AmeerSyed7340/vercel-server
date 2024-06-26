97a8c679d9b8b58caf0cc7ce3d4f17176f84ee0e
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-06-25 22:37:48 -0400

    put console.log in hello.js

--- server/api/hello.js
+++ server/api/hello.js
@@ -1,7 +1,1 @@
-// api/hello.js
-module.exports = (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-  };
-  
+console.log('hello.js') 
